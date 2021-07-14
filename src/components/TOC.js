c2764884d565a304a1ab12f0cf62ea525b36d671
Won Kilho <khwon@tsoft.co.kr>
2021-07-14 16:20:30 +0900

    15. state

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react'
 
 class TOC extends Component {
   render() {
+    var lists = []
+    var data = this.props.data
+    for (var i = 0; i < data.length; i++) {
+      lists.push(
+        <li key={data[i].id}>
+          <a href={'/content/' + data[i].id}>{data[i].title}</a>
+        </li>
+      )
+    }
     return (
       <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
+        <ul>{lists}</ul>
       </nav>
     )
   }
