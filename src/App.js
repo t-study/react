e12e546392a8c7c9a38287f867f04231ef582e0c
Won Kilho <khwon@tsoft.co.kr>
2021-07-14 15:06:59 +0900

    12.Pops

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ class Subject extends Component {
   render() {
     return (
       <header>
-        <h1>WEB</h1>
-        world wide web!
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     )
   }
@@ -35,8 +35,8 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <h2>HTML</h2>
-        HTML is HyperTextMarkup Language.
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
     )
   }
@@ -46,9 +46,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is HyperTextMarkup Language."></Content>
       </div>
     )
   }
