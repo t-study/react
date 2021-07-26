928c183b01906bd0925342fa7ea8a6f6c6ed9c65
Won Kilho <khwon@tsoft.co.kr>
2021-07-26 15:26:53 +0900

    04. React & Ajax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
+const Nav = ({ list, handleClick }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav>
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>
+            <a
+              href={item.id}
+              data-id={item.id}
+              onClick={(e) => {
+                e.preventDefault()
+                handleClick(e.target.dataset.id)
+              }}
+            >
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
-export default App;
+const Ariticle = ({ title, desc }) => {
+  return (
+    <article>
+      <h2>{title}</h2>
+      {desc}
+    </article>
+  )
+}
+
+const App = () => {
+  const [article, setArticle] = useState({ title: 'Welcome', desc: 'Hello React & Ajax' })
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    fetch('list.json')
+      .then((result) => result.json())
+      .then((json) => setList(json))
+  }, [])
+
+  const handleClick = (id) => {
+    fetch(id + '.json')
+      .then((result) => result.json())
+      .then((item) => setArticle(item))
+      .catch((e) => console.error(e))
+  }
+
+  return (
+    <div className='App'>
+      <h1>WEB</h1>
+      <Nav handleClick={(id) => handleClick(id)} list={list}></Nav>
+      <Ariticle title={article.title} desc={article.desc} />
+    </div>
+  )
+}
+
+export default App
